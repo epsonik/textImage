501555c800a16df916b0f0de6dde492fa5422a86
mateuszb <mateusz.b@aibusters.com>
2020-08-16 22:59:13 +0200

    -calculation of centroid

--- position_utils.py
+++ position_utils.py
@@ -1,6 +1,6 @@
 from BoundBox import BoundBox
 from img_det import get_labels
-
+import numpy as np
 
 def calculate_position(boxA: BoundBox, boxB: BoundBox):
 
@@ -12,3 +12,6 @@ def calculate_position(boxA: BoundBox, boxB: BoundBox):
     if(boxA.YtopLeft <= boxB.YbottomRight or boxB.YtopLeft <= boxA.YbottomRight):
         return "above", get_labels()[boxA.label], get_labels()[boxB.label]
     return False
+
+def calculate_centroid(coorA, coorB):
+    return np.double((coorB - coorA) / 2)
--- text_image.py
+++ text_image.py
@@ -1,13 +1,14 @@
 import pandas as pd
-
 from img_det import vbox_engine, draw_boxes
+from position_utils import calculate_centroid
 
 photo_filename = './IMAGES/dog.jpg'
 photo_boxed_filename = './dog_boxed.jpg'
 v_boxes, v_labels, v_scores = vbox_engine(photo_filename )
 img_id = id(v_boxes)
 draw_boxes(photo_filename, photo_boxed_filename, v_boxes, v_labels, v_scores)
-image_b_boxes = pd.DataFrame(columns = ['img_id','XtopLeft', 'YtopLeft', 'XbottomRight', 'YbottomRight','label'])
+columns = ['img_id','XtopLeft', 'YtopLeft', 'XbottomRight', 'YbottomRight','Xcentroid','Ycentroid','label']
+image_b_boxes = pd.DataFrame(columns =columns )
 
 
 for i in range(len(v_boxes)):
@@ -19,6 +20,8 @@ for i in range(len(v_boxes)):
                         'YtopLeft':[ymin],
                         'XbottomRight':[xmax],
                        'YbottomRight':[ymax],
+                       'Xcentroid':[calculate_centroid(xmin, xmax)],
+                        'Ycentroid':[calculate_centroid(ymin, ymax)],
                           'label':[label]})
     image_b_boxes = image_b_boxes.append(b_box, ignore_index=True)
 image_b_boxes.to_csv("dog.csv", index=False)
