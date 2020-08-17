5d4dec0c4e93443c716f2e67c6e17922febd3f1e
mateuszb <mateusz.b@aibusters.com>
2020-08-17 18:14:58 +0200

    -calculate centroids

--- position_utils.py
+++ position_utils.py
@@ -3,7 +3,7 @@ import numpy as np
 from BoundBox import BoundBox
 
 
-def calculate_position(boxA: BoundBox, boxB: BoundBox):
+def calculate_position_between_objects(boxA: BoundBox, boxB: BoundBox):
     # If one rectangle is on left side of other
     if (boxA.XtopLeft >= boxB.XbottomRight or boxB.XtopLeft >= boxA.XtopLeft):
         return "left"
@@ -16,3 +16,12 @@ def calculate_position(boxA: BoundBox, boxB: BoundBox):
 
 def calculate_centroid(coorA, length):
     return np.double((coorA) + length / 2)
+
+
+def centroid_distance(boxA, boxB):
+    return round(np.double(np.math.sqrt((boxB.Xcentroid - boxA.Xcentroid) ** 2 +
+                                        (boxB.Ycentroid - boxA.Ycentroid) ** 2)), 4)
+
+
+def position_on_image():
+    return True
--- process_from_file.py
+++ process_from_file.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 import BoundBox
-from position_utils import calculate_position
+from position_utils import calculate_position_between_objects, centroid_distance
 
 image_b_boxes_df = pd.read_csv("dog.csv")
 v_boxes = BoundBox.read_table(image_b_boxes_df)
+
 for i in range(len(v_boxes)):
     boxA = v_boxes[i]
     for b in range(len(v_boxes)):
         boxB = v_boxes[b]
         if i is not b:
-            print(calculate_position(boxA, boxB), )
+            print(calculate_position_between_objects(boxA, boxB), boxA.label, boxB.label, centroid_distance(boxA, boxB))
